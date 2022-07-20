ce99fda080caf2c95b946f1c2da6f64db6078a68
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2022-07-20 17:28:04 +0100

    Update for the next installment of the workshop.

--- .data/config.sh
+++ .data/config.sh
@@ -2,19 +2,19 @@
 ## WORKSHOP INFORMATION
 ##
 
-WEEKDAY=Thursday
-DAY=22
-MONTH=06
-YEAR=2022
+WEEKDAY=Wednesday
+DAY=18
+MONTH=01
+YEAR=2023
 
-ROOM="Lehar I"
-LOCATION="Budapest, HU"
-HIPEAC_LINK='https://www.hipeac.net/2022/budapest/#/'
+ROOM="TBD"
+LOCATION="Toulouse, FR"
+HIPEAC_LINK='https://www.hipeac.net/2023/toulouse/#/'
 
-PARMA=13
-DITAM=11
+PARMA=14
+DITAM=12
 
-SUBMISSION_LINK='https://easychair.org/conferences/?conf=parmaditam22'
+#SUBMISSION_LINK='https://easychair.org/conferences/?conf=parmaditam22'
 
 
 ##
@@ -22,14 +22,14 @@ SUBMISSION_LINK='https://easychair.org/conferences/?conf=parmaditam22'
 ## comment out items that should not appear
 ##
 
-INDEX_ITEM_CFP='<li><a href="./parma_ditam_2022_CfP.pdf">Call For Papers</a></li>'
+#INDEX_ITEM_CFP='<li><a href="./parma_ditam_2022_CfP.pdf">Call For Papers</a></li>'
 #INDEX_ITEM_CFP='<li>Call For Papers <b>TBA</b></li>'
 
-INDEX_ITEM_SUBMISSION='<li><a href="'"$SUBMISSION_LINK"'">Paper Submission</a></li>'
+#INDEX_ITEM_SUBMISSION='<li><a href="'"$SUBMISSION_LINK"'">Paper Submission</a></li>'
 #INDEX_ITEM_SUBMISSION='<li>Paper Submission <b>TBA</b></li>'
 
 #INDEX_ITEM_PROGRAM='<li><a href="./program.html">Workshop Program</a></li>'
-INDEX_ITEM_PROGRAM_PDF='<li><a href="./parma_ditam_2022_program.pdf">Workshop Program</a></li>'
+#INDEX_ITEM_PROGRAM_PDF='<li><a href="./parma_ditam_2022_program.pdf">Workshop Program</a></li>'
 
 
 ##
@@ -39,6 +39,6 @@ INDEX_ITEM_PROGRAM_PDF='<li><a href="./parma_ditam_2022_program.pdf">Workshop Pr
 ## formatted as extended deadlines. QUOTE EVERYTHING OR IT WON'T WORK
 ##
 
-SUBMISSION_DATES="23/11/2021 30/11/2021 25/12/2021 31/01/2022 11/02/2022"
-ACCEPTANCE_DATES="19/12/2021 16/01/2022 21/02/2022 11/03/2022"
-CAMERA_DATES="10/01/2022 23/02/2022 14/03/2022 01/04/2022"
+SUBMISSION_DATES="11/11/2022"
+ACCEPTANCE_DATES="09/12/2022"
+CAMERA_DATES="31/12/2022"
