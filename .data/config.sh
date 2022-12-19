38a5469eef245833a44fb59e6308d3556da5c5f4
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2022-12-19 21:10:09 +0100

    Add HTML version of the 2023 program.

--- .data/config.sh
+++ .data/config.sh
@@ -28,7 +28,8 @@ INDEX_ITEM_CFP='<li><a href="./parma_ditam_2023_CfP.pdf">Call For Papers</a></li
 INDEX_ITEM_SUBMISSION='<li><a href="'"$SUBMISSION_LINK"'">Paper Submission</a></li>'
 #INDEX_ITEM_SUBMISSION='<li>Paper Submission <b>TBA</b></li>'
 
-INDEX_ITEM_PROGRAM_PDF='<li><a href="./parma_ditam_2023_program_v4.pdf">Workshop Program</a></li>'
+INDEX_ITEM_PROGRAM='<li><a href="./program.html">Workshop Program</a></li>'
+INDEX_ITEM_PROGRAM_PDF='<li><a href="./parma_ditam_2023_program_v4.pdf">Workshop Program (PDF)</a></li>'
 
 
 ##
