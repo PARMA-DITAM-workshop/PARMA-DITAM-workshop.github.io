f0eb2a923f7471ab9677fc113a565258a97ffdba
SerenaC94 <serena.curzel@polimi.it>
2025-08-29 11:19:07 +0200

    add cfp, not under construction now

--- .data/config.sh
+++ .data/config.sh
@@ -22,11 +22,11 @@ SUBMISSION_LINK='https://easychair.org/conferences?conf=parmaditam2026'
 ## comment out items that should not appear
 ##
 
-# INDEX_ITEM_CFP='<li><a href="./parma_ditam_2025_CfP.pdf">Call For Papers</a></li>'
-INDEX_ITEM_CFP='<li>Call For Papers <b>TBA</b></li>'
+INDEX_ITEM_CFP='<li><a href="./parma_ditam_2026_CfP.pdf">Call For Papers</a></li>'
+# INDEX_ITEM_CFP='<li>Call For Papers <b>TBA</b></li>'
 
-# INDEX_ITEM_SUBMISSION='<li><a href="'"$SUBMISSION_LINK"'">Paper Submission</a></li>'
-INDEX_ITEM_SUBMISSION='<li>Paper Submission <b>TBA</b></li>'
+INDEX_ITEM_SUBMISSION='<li><a href="'"$SUBMISSION_LINK"'">Paper Submission</a></li>'
+# INDEX_ITEM_SUBMISSION='<li>Paper Submission <b>TBA</b></li>'
 
 INDEX_ITEM_PROGRAM='<li>Workshop Program  <b>TBA</b></li>'
 INDEX_ITEM_PROGRAM_PDF='<li>Workshop Program (PDF) <b>TBA</b></li>'
@@ -40,6 +40,6 @@ INDEX_ITEM_PROGRAM_PDF='<li>Workshop Program (PDF) <b>TBA</b></li>'
 ## formatted as extended deadlines. QUOTE EVERYTHING OR IT WON'T WORK
 ##
 
-SUBMISSION_DATES="TBA" # "17/11/2023 03/12/2023"
-ACCEPTANCE_DATES="TBA" # "12/12/2023 22/12/2023"
-CAMERA_DATES="TBA" # "31/12/2023 08/01/2024"
+SUBMISSION_DATES="14/11/2025" # "17/11/2023 03/12/2023"
+ACCEPTANCE_DATES="12/12/2025" # "12/12/2023 22/12/2023"
+CAMERA_DATES="08/01/2026" # "31/12/2023 08/01/2024"
