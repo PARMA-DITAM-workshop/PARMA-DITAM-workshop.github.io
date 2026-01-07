2b091874b90e45a0df821741b17d4811df5c459d
SerenaC94 <serena.curzel@polimi.it>
2026-01-07 09:45:49 +0100

    update camera-ready deadline

--- .data/config.sh
+++ .data/config.sh
@@ -28,7 +28,7 @@ INDEX_ITEM_CFP='<li><a href="./parma_ditam_2026_CfP.pdf">Call For Papers</a></li
 INDEX_ITEM_SUBMISSION='<li><a href="'"$SUBMISSION_LINK"'">Paper Submission</a></li>'
 # INDEX_ITEM_SUBMISSION='<li>Paper Submission <b>TBA</b></li>'
 
-INDEX_ITEM_PROGRAM='<li>Workshop Program  <b>TBA</b></li>'
+INDEX_ITEM_PROGRAM='<li><a href="./program.html">Workshop Program (Tentative)</a></li>'
 INDEX_ITEM_PROGRAM_PDF='<li>Workshop Program (PDF) <b>TBA</b></li>'
 # INDEX_ITEM_PROGRAM='<li><a href="./program.html">Workshop Program</a></li>'
 # INDEX_ITEM_PROGRAM_PDF='<li><a href="./parma_ditam_2025_program.pdf">Workshop Program (PDF)</a></li>'
@@ -42,4 +42,4 @@ INDEX_ITEM_PROGRAM_PDF='<li>Workshop Program (PDF) <b>TBA</b></li>'
 
 SUBMISSION_DATES="14/11/2025 28/11/2025" # 03/12/2023"
 ACCEPTANCE_DATES="12/12/2025 19/12/2025" # 22/12/2023"
-CAMERA_DATES="08/01/2026" # "31/12/2023 08/01/2024"
+CAMERA_DATES="08/01/2026 19/01/2026" # "31/12/2023 08/01/2024"
