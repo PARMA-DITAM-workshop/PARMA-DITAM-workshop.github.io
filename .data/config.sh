04a5c5b17d2638802a892730ea778c7ece45f4f3
SerenaC94 <serena.curzel@polimi.it>
2026-06-26 16:02:55 +0200

    2027 website

--- .data/config.sh
+++ .data/config.sh
@@ -2,17 +2,17 @@
 ## WORKSHOP INFORMATION
 ##
 
-WEEKDAY=Tuesday
-DAY=27
+WEEKDAY=Monday
+DAY=18
 MONTH=01
-YEAR=2026
+YEAR=2027
 
-ROOM="S4A"
-LOCATION="Krakow, Poland"
-HIPEAC_LINK='https://www.hipeac.net/2026/krakow/#/'
+ROOM="Carron 1"
+LOCATION="Glasgow, Scotland"
+HIPEAC_LINK='https://www.hipeac.net/2027/glasgow/#/'
 
-PARMA=17
-DITAM=15
+PARMA=18
+DITAM=16
 
 SUBMISSION_LINK='https://easychair.org/conferences?conf=parmaditam2026'
 
@@ -22,14 +22,14 @@ SUBMISSION_LINK='https://easychair.org/conferences?conf=parmaditam2026'
 ## comment out items that should not appear
 ##
 
-INDEX_ITEM_CFP='<li><a href="./parma_ditam_2026_CfP.pdf">Call For Papers</a></li>'
-# INDEX_ITEM_CFP='<li>Call For Papers <b>TBA</b></li>'
+# INDEX_ITEM_CFP='<li><a href="./parma_ditam_2026_CfP.pdf">Call For Papers</a></li>'
+INDEX_ITEM_CFP='<li>Call For Papers <b>TBA</b></li>'
 
-INDEX_ITEM_SUBMISSION='<li><a href="'"$SUBMISSION_LINK"'">Paper Submission</a></li>'
-# INDEX_ITEM_SUBMISSION='<li>Paper Submission <b>TBA</b></li>'
+# INDEX_ITEM_SUBMISSION='<li><a href="'"$SUBMISSION_LINK"'">Paper Submission</a></li>'
+INDEX_ITEM_SUBMISSION='<li>Paper Submission <b>TBA</b></li>'
 
-INDEX_ITEM_PROGRAM='<li><a href="./program.html">Workshop Program (Tentative)</a></li>'
-INDEX_ITEM_PROGRAM_PDF='<li><a href="./parma_ditam_2026_program.pdf">Workshop Program (PDF)</a></li>'
+INDEX_ITEM_PROGRAM='<li>Workshop Program  <b>TBA</b></li>'
+INDEX_ITEM_PROGRAM_PDF='<li>Workshop Program (PDF) <b>TBA</b></li>'
 # INDEX_ITEM_PROGRAM='<li><a href="./program.html">Workshop Program</a></li>'
 # INDEX_ITEM_PROGRAM_PDF='<li><a href="./parma_ditam_2025_program.pdf">Workshop Program (PDF)</a></li>'
 
@@ -40,6 +40,6 @@ INDEX_ITEM_PROGRAM_PDF='<li><a href="./parma_ditam_2026_program.pdf">Workshop Pr
 ## formatted as extended deadlines. QUOTE EVERYTHING OR IT WON'T WORK
 ##
 
-SUBMISSION_DATES="14/11/2025 28/11/2025" # 03/12/2023"
-ACCEPTANCE_DATES="12/12/2025 19/12/2025" # 22/12/2023"
-CAMERA_DATES="08/01/2026 19/01/2026" # "31/12/2023 08/01/2024"
+SUBMISSION_DATES="TBA" # 03/12/2023"
+ACCEPTANCE_DATES="TBA" # 22/12/2023"
+CAMERA_DATES="TBA" # "31/12/2023 08/01/2024"
